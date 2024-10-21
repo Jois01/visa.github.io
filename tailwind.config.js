a8601caecda6e5084f0d2f39c92c2c33c0df6f04
Jois <putujois0703@gmail.com>
2024-10-21 17:29:19 +0800

    merubah text, perbaiki tata letak, dan menambah dropdown

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      
+    },
+    fontFamily: {
+      'mono': ['"Ubuntu Mono", monospace'],
+      'serif': ['"Plus Jakarta Sans", sans-serif;'],
+    },
+   
   },
   plugins: [],
 }
